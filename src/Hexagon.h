df7f4b04c850c761b3d774f771011e047f2b0bf8
dawoczek <d.glazewski04@gmail.com>
2026-03-08 11:04:27 +0100

    gitignore

--- src/Hexagon.h
+++ src/Hexagon.h
@@ -1,4 +1,4 @@
-#ifndef _HEXAGON_H
+#ifdef _HEXAGON_H
 #define _HEXAGON_H
 #include <cmath>
 
@@ -8,18 +8,16 @@
 using namespace std;
 #include "ShapeParam.h"
 
-template<class T>
-class Hexagon : public Shape2D<T> {
-  public:
+template <class T> class Hexagon : public Shape2D<T> {
+public:
     inline ShapeResult<T> compute();
 
     inline string print();
 
-    inline Hexagon(const ShapeParam<T> & param);
-
+    inline Hexagon(const ShapeParam<T>& param);
 };
-template<class T>
-inline ShapeResult<T> Hexagon<T>::compute() {
+template <class T> inline ShapeResult<T> Hexagon<T>::compute()
+{
 
     ShapeResult<T> result;
     T side = this->m_param.get_attrib(PARAM_WIDTH);
@@ -33,16 +31,16 @@ inline ShapeResult<T> Hexagon<T>::compute() {
     return result;
 }
 
-template<class T>
-inline string Hexagon<T>::print() {
+template <class T> inline string Hexagon<T>::print()
+{
 
-  T side = this->m_param.get_attrib(PARAM_WIDTH);
+    T side = this->m_param.get_attrib(PARAM_WIDTH);
 
-  return "Hexagon side=" + to_string(side);
+    return "Hexagon side=" + to_string(side);
 }
 
-template<class T>
-inline Hexagon<T>::Hexagon(const ShapeParam<T> & param) : Shape2D<T>(param) {
-}
+template <class T>
+inline Hexagon<T>::Hexagon(const ShapeParam<T>& param): Shape2D<T>(param)
+{}
 
 #endif
