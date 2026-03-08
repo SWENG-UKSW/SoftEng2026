69ef0e38a031e7678868b77b3092dd1d69fa3f71
Jan Maciążek <jmaciazek-mac@MacBook-Pro-Jan.local>
2026-03-08 10:41:48 +0100

    Format Fix

--- src/Cylinder.h
+++ src/Cylinder.h
@@ -17,7 +17,7 @@ class Cylinder : public Shape3D<T> {
 
     inline string print();
 
-    inline Cylinder(const ShapeParam<T> & param);
+    inline Cylinder(const ShapeParam<T>& param);
 };
 template<class T>
 inline ShapeResult<T> Cylinder<T>::compute() {
@@ -53,7 +53,7 @@ inline string Cylinder<T>::print() {
 }
 
 template<class T>
-inline Cylinder<T>::Cylinder(const ShapeParam<T> & param) : Shape3D<T>(param) {
+inline Cylinder<T>::Cylinder(const ShapeParam<T>& param) : Shape3D<T>(param) {
 }
 
 #endif
