4e4495df068ae4f9b314c77d936616b036ed12f1
131143 <dyziek2002@wp.pl>
2026-03-08 09:32:36 +0100

    Rozwiązanie konfliktu sphere znowu

--- src/Sphere.h
+++ src/Sphere.h
@@ -12,32 +12,37 @@ using namespace std;
 template<class T>
 class Sphere : public Shape3D<T> {
 public:
-    inline virtual ShapeResultData<T> compute();
-
+    inline virtual ShapeResult<T> compute();
     inline string print();
-
     inline Sphere(const ShapeParam<T>& param);
 };
 
 template<class T>
-inline ShapeResultData<T> Sphere<T>::compute() {
-
-    T r = this->param.get(0);
+inline ShapeResult<T> Sphere<T>::compute()
+{
+    T r = this->m_param.get_attrib(ShapeParamIndex(0));
 
     T volume = (4.0 / 3.0) * 3.141592653589793 * r * r * r;
     T surface = 4 * 3.141592653589793 * r * r;
 
-    return ShapeResultData<T>(surface, volume);
+    ShapeResult<T> result;
+
+    result.set_attrib(ShapeResultIndex(0), surface);
+    result.set_attrib(ShapeResultIndex(1), volume);
+
+    return result;
 }
 
 template<class T>
-inline string Sphere<T>::print() {
+inline string Sphere<T>::print()
+{
     return "Sphere";
 }
 
 template<class T>
 inline Sphere<T>::Sphere(const ShapeParam<T>& param)
-    : Shape3D<T>(param) {
+    : Shape3D<T>(param)
+{
 }
 
 #endif
