caa92e3283183de46e69575d15489209f89db698
Oskar Szafraniec <129787@uksw.edu.pl>
2026-03-08 11:13:06 +0100

    naprawa3

--- src/Ellipse.h
+++ src/Ellipse.h
@@ -10,30 +10,30 @@ using namespace std;
 template<class T>
 class Ellipse : public Shape2D<T> {
 public:
-    inline ShapeResult<T> compute();
-    inline string print();
-    inline Ellipse(const ShapeParam<T>& param);
+	inline ShapeResult<T> compute();
+	inline string print();
+	inline Ellipse(const ShapeParam<T>& param);
 };
 template<class T>
 inline ShapeResult<T> Ellipse<T>::compute() {
-    T w = this->m_param.get_attrib(ShapeParamIndex::PARAM_WIDTH);
-    T h = this->m_param.get_attrib(ShapeParamIndex::PARAM_HEIGHT);
+	T w = this->m_param.get_attrib(ShapeParamIndex::PARAM_WIDTH);
+	T h = this->m_param.get_attrib(ShapeParamIndex::PARAM_HEIGHT);
 
-    T a = w / (T)2;
-    T b = h / (T)2;
+	T a = w / (T)2;
+	T b = h / (T)2;
 
-    ShapeResult<T> res;
+	ShapeResult<T> res;
 
-    const double PI = 3.14159265358979323846;
-    res.set_attrib(ShapeResultIndex::RESULT_AREA, (T)(PI * a * b));
+	const double PI = 3.14159265358979323846;
+	res.set_attrib(ShapeResultIndex::RESULT_AREA, (T)(PI * a * b));
 
-    return res;
+	return res;
 }
 template<class T>
 inline string Ellipse<T>::print() {
-    T w = this->m_param.get_attrib(ShapeParamIndex::PARAM_WIDTH);
-    T h = this->m_param.get_attrib(ShapeParamIndex::PARAM_HEIGHT);
-    return "Ellipse(" + to_string(w) + ", " + to_string(h) + ")";
+	T w = this->m_param.get_attrib(ShapeParamIndex::PARAM_WIDTH);
+	T h = this->m_param.get_attrib(ShapeParamIndex::PARAM_HEIGHT);
+	return "Ellipse(" + to_string(w) + ", " + to_string(h) + ")";
 }
 template <class T>
 inline Ellipse<T>::Ellipse(const ShapeParam<T>& param) : Shape2D<T>(param)
