32dd5b2d0764bb9f08177faf6f8bf44337f9b8b4
dawoczek <d.glazewski04@gmail.com>
2026-03-20 20:16:10 +0100

    Remove build directory

--- build/externals/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Dawid/Desktop/SoftEng2026/SoftEng2026/externals/googletest
-# Build directory: C:/Users/Dawid/Desktop/SoftEng2026/SoftEng2026/build/externals/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
