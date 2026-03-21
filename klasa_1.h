561441edeeff6b6982911c8a2245f41807f71e00
Jakub131143 <jakubniezgodzinski@gmail.com>
2026-03-21 21:06:53 +0100

    Main.yml changes in 131143and129714 branch

---
+++ klasa_1.h
@@ -0,0 +1,10 @@
+#ifndef _KLASA_1_H
+#define _KLASA_1_H
+
+
+#include "klasa_2.h"
+
+template<class klasunia = klasa_1<klasunia> >
+class klasa_1 : public klasa_2 {
+};
+#endif
---
+++ klasa_3.cpp
@@ -0,0 +1,5 @@
+
+#include "klasa_3.h"
+#include "klasa_1.h"
+#include "klasa_2.h"
+
