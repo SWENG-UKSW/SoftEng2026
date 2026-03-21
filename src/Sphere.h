0fdd75b5762261a549a57ba5b76f05ea94eaa449
Jakub131143 <jakubniezgodzinski@gmail.com>
2026-03-21 23:23:17 +0100

    Fix sphere

--- src/Sphere.h
+++ src/Sphere.h
@@ -9,46 +9,39 @@
 
 using namespace std;
 
-template<class T>
-class Sphere : public Shape3D<T> {
+template <class T> class Sphere : public Shape3D<T> {
 public:
     inline virtual ShapeResult<T> compute();
     inline string print();
     inline Sphere(const ShapeParam<T>& param);
 };
 
-template<class T>
-inline ShapeResult<T> Sphere<T>::compute()
+template <class T> inline ShapeResult<T> Sphere<T>::compute()
 {
     T r = this->m_param.get_attrib(ShapeParamIndex(0));
 
-ShapeResult<T> result;
+    ShapeResult<T> result;
 
-if (r <= 0) {
-    result.set_attrib(ShapeResultIndex(0), 0);
-    result.set_attrib(ShapeResultIndex(1), 0);
+    if (r <= 0)
+    {
+        result.set_attrib(ShapeResultIndex(0), 0);
+        result.set_attrib(ShapeResultIndex(1), 0);
+        return result;
+    }
+
+    T volume = (4.0 / 3.0) * 3.141592653589793 * r * r * r;
+    T surface = 4 * 3.141592653589793 * r * r;
+
+    result.set_attrib(ShapeResultIndex(0), surface);
+    result.set_attrib(ShapeResultIndex(1), volume);
+
     return result;
 }
 
-T volume = (4.0 / 3.0) * 3.141592653589793 * r * r * r;
-T surface = 4 * 3.141592653589793 * r * r;
+template <class T> inline string Sphere<T>::print() { return "Sphere"; }
 
-result.set_attrib(ShapeResultIndex(0), surface);
-result.set_attrib(ShapeResultIndex(1), volume);
-
-return result;
-}
-
-template<class T>
-inline string Sphere<T>::print()
-{
-    return "Sphere";
-}
-
-template<class T>
-inline Sphere<T>::Sphere(const ShapeParam<T>& param)
-    : Shape3D<T>(param)
-{
-}
+template <class T>
+inline Sphere<T>::Sphere(const ShapeParam<T>& param): Shape3D<T>(param)
+{}
 
 #endif
