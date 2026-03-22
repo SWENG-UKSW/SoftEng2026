f627259ad23dbf5e7ee902fbaac89a787648e97a
131143 <dyziek2002@wp.pl>
2026-03-22 12:59:51 +0100

    move tests to external file

--- src/tests/smoke/test_shape1.cpp
+++ src/tests/smoke/test_shape1.cpp
@@ -1,106 +1,38 @@
+
 #define _USE_MATH_DEFINES
 #include <cmath>
-#include <memory>
 #include <gtest/gtest.h>
 #include "ShapeFactory.h"
-#include <limits>
 
-TEST(test_sphere, HappyPath)
+TEST(test_shape1, AddFunction)
 {
+    bool res = true;
     ShapeParam<float> param;
+    res = param.set_attrib(ShapeParamIndex::PARAM_RADIUS, 1.f);
+    ASSERT_NE(res, false);
 
-    ASSERT_TRUE(param.set_attrib(ShapeParamIndex::PARAM_RADIUS, 1.f));
-    param.type = ShapeType::PT_SPHERE;
+    param.type = ShapeType::PT_CIRCLE;
 
-    ASSERT_TRUE(param.validate());
+    res = param.validate();
+    ASSERT_NE(res, false);
 
     auto shape =
         std::unique_ptr<IShape<float>>(ShapeFactory<float>::create(param));
     ASSERT_NE(shape, nullptr);
 
-    ShapeResult<float> data = shape->compute();
+#if 0
+    ShapeResult<float> data=shape->compute();
+    float area=data.get_attrib(ShapeResultIndex::RESULT_AREA);
+    ASSERT_NE(area, 0.f);
+#endif
 
-    float surface = data.get_attrib(ShapeResultIndex(0));
-    float volume = data.get_attrib(ShapeResultIndex(1));
-
-    ASSERT_NEAR(surface, 4 * M_PI, 0.001f);
-    ASSERT_NEAR(volume, (4.0f / 3.0f) * M_PI, 0.001f);
+    shape.reset(nullptr);
+    ASSERT_EQ(shape.get(), nullptr);
 }
 
-TEST(test_sphere, ZeroRadius)
+
+int main(int argc, char** argv)
 {
-    ShapeParam<float> param;
-
-    param.set_attrib(ShapeParamIndex::PARAM_RADIUS, 0.f);
-    param.type = ShapeType::PT_SPHERE;
-
-    auto shape =
-        std::unique_ptr<IShape<float>>(ShapeFactory<float>::create(param));
-    ASSERT_NE(shape, nullptr);
-
-    ShapeResult<float> data = shape->compute();
-
-    ASSERT_EQ(data.get_attrib(ShapeResultIndex(0)), 0.f);
-    ASSERT_EQ(data.get_attrib(ShapeResultIndex(1)), 0.f);
-}
-
-TEST(test_sphere, NegativeRadius)
-{
-    ShapeParam<float> param;
-
-    param.set_attrib(ShapeParamIndex::PARAM_RADIUS, -5.f);
-    param.type = ShapeType::PT_SPHERE;
-
-    auto shape =
-        std::unique_ptr<IShape<float>>(ShapeFactory<float>::create(param));
-    ASSERT_NE(shape, nullptr);
-
-    ShapeResult<float> data = shape->compute();
-
-    ASSERT_EQ(data.get_attrib(ShapeResultIndex(0)), 0.f);
-    ASSERT_EQ(data.get_attrib(ShapeResultIndex(1)), 0.f);
-}
-
-TEST(test_sphere, MaxFloat)
-{
-    ShapeParam<float> param;
-
-    float max = std::numeric_limits<float>::max();
-
-    param.set_attrib(ShapeParamIndex::PARAM_RADIUS, max);
-    param.type = ShapeType::PT_SPHERE;
-
-    ASSERT_TRUE(param.validate());
-
-    auto shape =
-        std::unique_ptr<IShape<float>>(ShapeFactory<float>::create(param));
-    ASSERT_NE(shape, nullptr);
-
-    ShapeResult<float> data = shape->compute();
-
-    float surface = data.get_attrib(ShapeResultIndex(0));
-    float volume = data.get_attrib(ShapeResultIndex(1));
-
-    ASSERT_TRUE(std::isinf(surface) || std::isfinite(surface));
-    ASSERT_TRUE(std::isinf(volume) || std::isfinite(volume));
-}
-
-TEST(test_sphere, SmallRadiusPrecision)
-{
-    ShapeParam<float> param;
-
-    param.set_attrib(ShapeParamIndex::PARAM_RADIUS, 0.001f);
-    param.type = ShapeType::PT_SPHERE;
-
-    ASSERT_TRUE(param.validate());
-
-    auto shape =
-        std::unique_ptr<IShape<float>>(ShapeFactory<float>::create(param));
-    ASSERT_NE(shape, nullptr);
-
-    ShapeResult<float> data = shape->compute();
-
-    float surface = data.get_attrib(ShapeResultIndex(0));
-
-    ASSERT_NEAR(surface, 4 * M_PI * 0.000001f, 0.000001f);
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
