3676767d3bafe9f2ab685061260f158265bffcb2
Mateusz Gołębiowski <129696@uksw.edu.pl>
2026-04-26 13:01:48 +0200

    Fix1.1

--- src/tests/smoke/test_shape1.cpp
+++ src/tests/smoke/test_shape1.cpp
@@ -2,22 +2,20 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 #include <gtest/gtest.h>
-#include <chrono> // Do pomiaru czasu
+#include <chrono> 
 #include "ShapeFactory.h"
 
-// TEST 1: Wydajnoœæ i Pamiêæ (unique_ptr)
 TEST(test_shape1, PerformanceAndMemoryTest)
 {
     ShapeParam<double> param;
     param.set_attrib(ShapeParamIndex::PARAM_RADIUS, 1.0);
     param.type = ShapeType::PT_CIRCLE;
 
-    // unique_ptr automatycznie sprz¹ta pamiêæ - brak wycieków! [cite: 461]
     auto shape =
         std::unique_ptr<IShape<double>>(ShapeFactory<double>::create(param));
     ASSERT_NE(shape, nullptr);
 
-    // Pomiar czasu: 100 iteracji [cite: 462]
+
     auto start = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < 100; ++i)
     {
@@ -28,14 +26,11 @@ TEST(test_shape1, PerformanceAndMemoryTest)
     auto duration =
         std::chrono::duration_cast<std::chrono::microseconds>(end - start)
             .count();
-    // Warunek: czas < 1ms (1000 mikrosekund) [cite: 462]
     EXPECT_LT(duration, 1000);
 }
 
-// TEST 2: Obs³uga ró¿nych typów danych (Template) [cite: 462]
 TEST(test_shape1, MultiTypeSupportTest)
 {
-    // Sprawdzamy typ float
     ShapeParam<float> pFloat;
     pFloat.type = ShapeType::PT_CIRCLE;
     pFloat.set_attrib(ShapeParamIndex::PARAM_RADIUS, 2.0f);
