cecc94e1e8d7250ae52be281915ac68dbaffcd54
Gjentki <85639883+Gjentki@users.noreply.github.com>
2026-04-26 12:18:34 +0200

    Update Cone.h

--- src/Cone.h
+++ src/Cone.h
@@ -1,35 +1,66 @@
 #ifndef _CONE_H
 #define _CONE_H
 
-
 #include "Shape3D.h"
 #include "ShapeResultData.h"
-#include <string>
-using namespace std;
 #include "ShapeParam.h"
+#include <string>
+#include <cmath>
+#include <sstream>
 
-template<class T>
-class Cone : public Shape3D<T> {
-  public:
-    inline virtual ShapeResult<T> compute();
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
-    inline string print();
+using namespace std;
 
-    inline Cone(const ShapeParam<T> & param);
-
+template <class T> class Cone : public Shape3D<T> {
+public:
+    inline ShapeResult<T> compute() override;
+    inline string print() override;
+    inline Cone(const ShapeParam<T>& param);
 };
-template<class T>
-inline ShapeResult<T> Cone<T>::compute() {
-  return ShapeResult<T>();
+
+template <class T> inline ShapeResult<T> Cone<T>::compute()
+{
+    this->m_param.validate(); // Wywołanie poprawionej walidacji
+
+    T radius = this->m_param.get_attrib(PARAM_RADIUS);
+    T height = this->m_param.get_attrib(PARAM_HEIGHT);
+
+    double r = static_cast<double>(radius);
+    double h = static_cast<double>(height);
+
+    double volume = (M_PI * r * r * h) / 3.0;
+    double slantHeight = sqrt(r * r + h * h);
+    double surface = M_PI * r * (r + slantHeight);
+
+    ShapeResult<T> result;
+    result.set_attrib(RESULT_VOLUME, static_cast<T>(volume));
+    result.set_attrib(RESULT_SURFACE, static_cast<T>(surface));
+
+    return result;
 }
 
-template<class T>
-inline string Cone<T>::print() {
-  return "";
+template <class T> inline string Cone<T>::print()
+{
+    T radius = this->m_param.get_attrib(PARAM_RADIUS);
+    T height = this->m_param.get_attrib(PARAM_HEIGHT);
+    ShapeResult<T> result = compute();
+
+    ostringstream out;
+    out << "=== SOLID: CONE ===" << endl;
+    out << "Radius: " << radius << endl;
+    out << "Height: " << height << endl;
+    out << "Volume: " << result.get_attrib(RESULT_VOLUME) << endl;
+    out << "Surface: " << result.get_attrib(RESULT_SURFACE) << endl;
+    out << "=====================";
+
+    return out.str();
 }
 
-template<class T>
-inline Cone<T>::Cone(const ShapeParam<T> & param) : Shape3D<T>(param) {
-}
+template <class T>
+inline Cone<T>::Cone(const ShapeParam<T>& param): Shape3D<T>(param)
+{}
 
 #endif
