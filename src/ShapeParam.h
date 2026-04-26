62874863e3c54cf77c71c36a33f22b2f29a8ec08
Eryk Nowacki <129730@uksw.edu.pl>
2026-04-26 12:01:41 +0200

    Poprawki do metody ShapeParam::validate

--- src/ShapeParam.h
+++ src/ShapeParam.h
@@ -1,44 +1,39 @@
 #ifndef _SHAPEPARAM_H
 #define _SHAPEPARAM_H
 
-
 #include <vector>
-using namespace std;
+#include <stdexcept> 
 #include "ShapeType.h"
 #include "ShapeParamIndex.h"
 
-template<class T>
-class ShapeParam {
-  private:
-    vector<T> attribs;
+template <class T> class ShapeParam {
+private:
+    std::vector<T> attribs;
 
-
-  public:
+public:
     ShapeType type;
 
-    inline T get_attrib(ShapeParamIndex ind) const;
+    inline T get_attrib(size_t ind) const
+    {
+        if (ind >= attribs.size())
+        {
+            throw std::out_of_range("Indeks poza zakresem parametrów kszta³tu");
+        }
+        return attribs[ind];
+    }
 
-    inline bool set_attrib(ShapeParamIndex ind, const T & val);
+    inline bool set_attrib(size_t ind, const T& val)
+    {
+        if (ind >= attribs.size())
+        {
+            attribs.resize(ind + 1);
+        }
+        attribs[ind] = val;
+        return true;
+    }
 
     inline bool validate() const;
-
 };
-template<class T>
-inline T ShapeParam<T>::get_attrib(ShapeParamIndex ind) const {
-    if(ind<attribs.size()) return attribs[ind];
-    return 0;
-}
 
-template<class T>
-inline bool ShapeParam<T>::set_attrib(ShapeParamIndex ind, const T & val) {
-  if(ind>=attribs.size())attribs.resize(ind+1);
-  attribs[ind]=val;
-  return true;
-}
-
-template<class T>
-inline bool ShapeParam<T>::validate() const {
-  return true;
-}
 
 #endif
