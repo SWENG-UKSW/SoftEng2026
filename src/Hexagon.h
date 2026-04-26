c7a345b5eef4e71e1559f453fe79dcb0118307dd
dawoczek <d.glazewski04@gmail.com>
2026-04-26 12:28:47 +0200

    shape implementation - poprawka

--- src/Hexagon.h
+++ src/Hexagon.h
@@ -1,35 +1,52 @@
 #ifndef _HEXAGON_H
 #define _HEXAGON_H
 
-
+#include <cmath>
+#include <string>
 #include "Shape2D.h"
 #include "ShapeResultData.h"
-#include <string>
-using namespace std;
+#include "ShapeResultIndex.h"
 #include "ShapeParam.h"
+#include "ShapeParamIndex.h"
 
-template<class T>
-class Hexagon : public Shape2D<T> {
-  public:
+using namespace std;
+
+template <class T> class Hexagon : public Shape2D<T> {
+public:
     inline ShapeResult<T> compute();
-
     inline string print();
-
-    inline Hexagon(const ShapeParam<T> & param);
-
+    inline Hexagon(const ShapeParam<T>& param);
 };
-template<class T>
-inline ShapeResult<T> Hexagon<T>::compute() {
-  return ShapeResult<T>();
+
+template <class T> inline ShapeResult<T> Hexagon<T>::compute()
+{
+    ShapeResult<T> result;
+    // Zak³adamy, ¿e u¿ywamy szerokoœci (width) jako boku szeœciok¹ta
+    T side = this->m_param.get_attrib(PARAM_WIDTH);
+
+    // Wzór na pole szeœciok¹ta foremnego: (3 * sqrt(3) / 2) * a^2
+    T area = static_cast<T>((3.0 * sqrt(3.0) / 2.0) * side * side);
+    T perimeter = static_cast<T>(6.0 * side);
+
+    result.set_attrib(RESULT_AREA, area);
+    result.set_attrib(RESULT_PERIMETER, perimeter);
+
+    return result;
 }
 
-template<class T>
-inline string Hexagon<T>::print() {
-  return "";
+template <class T> inline string Hexagon<T>::print()
+{
+    ShapeResult<T> result = compute();
+    T area = result.get_attrib(RESULT_AREA);
+    T perimeter = result.get_attrib(RESULT_PERIMETER);
+    T side = this->m_param.get_attrib(PARAM_WIDTH);
+
+    return "Hexagon (side=" + to_string(side) + ") | area: " + to_string(area)
+        + ", perimeter: " + to_string(perimeter);
 }
 
-template<class T>
-inline Hexagon<T>::Hexagon(const ShapeParam<T> & param) : Shape2D<T>(param) {
-}
+template <class T>
+inline Hexagon<T>::Hexagon(const ShapeParam<T>& param): Shape2D<T>(param)
+{}
 
 #endif
