ada9f56c34e3311bafdbfc203ac917dea0e10cd8
Oskar Szafraniec <129787@uksw.edu.pl>
2026-04-26 13:01:17 +0200

    Poprawa formatowania pliku ShapeParam.h

--- src/ShapeParam.h
+++ src/ShapeParam.h
@@ -7,33 +7,32 @@ using namespace std;
 #include "ShapeType.h"
 #include "ShapeParamIndex.h"
 
-template<class T>
-class ShapeParam {
-  private:
+template <class T> class ShapeParam {
+private:
     vector<T> attribs;
 
 
-  public:
+public:
     ShapeType type;
 
     inline T get_attrib(ShapeParamIndex ind) const;
 
-    inline bool set_attrib(ShapeParamIndex ind, const T & val);
+    inline bool set_attrib(ShapeParamIndex ind, const T& val);
 
     inline bool validate() const;
-
 };
-template<class T>
-inline T ShapeParam<T>::get_attrib(ShapeParamIndex ind) const {
-    if(ind<attribs.size()) return attribs[ind];
+template <class T> inline T ShapeParam<T>::get_attrib(ShapeParamIndex ind) const
+{
+    if (ind < attribs.size()) return attribs[ind];
     return 0;
 }
 
-template<class T>
-inline bool ShapeParam<T>::set_attrib(ShapeParamIndex ind, const T & val) {
-  if(ind>=attribs.size())attribs.resize(ind+1);
-  attribs[ind]=val;
-  return true;
+template <class T>
+inline bool ShapeParam<T>::set_attrib(ShapeParamIndex ind, const T& val)
+{
+    if (ind >= attribs.size()) attribs.resize(ind + 1);
+    attribs[ind] = val;
+    return true;
 }
 
 template <class T> inline bool ShapeParam<T>::validate() const
@@ -52,7 +51,7 @@ template <class T> inline bool ShapeParam<T>::validate() const
         default: break;
     }
 
-    return true; 
+    return true;
 }
 
 #endif
